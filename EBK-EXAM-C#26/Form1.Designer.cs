eb122d703931c394b1ac379bab4230acd65f86a4
FLIPPED-RN <rnazarov142@gmail.com>
2026-05-26 11:50:18 +0200

    Update

--- EBK-EXAM-C#26/Form1.Designer.cs
+++ EBK-EXAM-C#26/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EBK_EXAM_C_26
 {
-    partial class Логин
+    partial class LoginForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,14 +29,16 @@
         private void InitializeComponent()
         {
             button1 = new Button();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
+            textBoxLogin = new TextBox();
+            textBoxPassword = new TextBox();
             label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(85, 203);
+            button1.Location = new Point(87, 233);
             button1.Name = "button1";
             button1.Size = new Size(187, 34);
             button1.TabIndex = 0;
@@ -44,41 +46,63 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
-            // textBox1
+            // textBoxLogin
             // 
-            textBox1.Location = new Point(43, 104);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(269, 23);
-            textBox1.TabIndex = 1;
+            textBoxLogin.Location = new Point(43, 121);
+            textBoxLogin.Name = "textBoxLogin";
+            textBoxLogin.Size = new Size(269, 23);
+            textBoxLogin.TabIndex = 1;
             // 
-            // textBox2
+            // textBoxPassword
             // 
-            textBox2.Location = new Point(43, 157);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(269, 23);
-            textBox2.TabIndex = 2;
+            textBoxPassword.Location = new Point(43, 177);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.Size = new Size(269, 23);
+            textBoxPassword.TabIndex = 2;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 20F);
-            label1.Location = new Point(132, 44);
+            label1.Location = new Point(141, 43);
             label1.Name = "label1";
-            label1.Size = new Size(93, 37);
+            label1.Size = new Size(75, 37);
             label1.TabIndex = 3;
-            label1.Text = "Логин";
+            label1.Text = "Вход";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(43, 97);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 21);
+            label2.TabIndex = 4;
+            label2.Text = "Логин";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F);
+            label3.Location = new Point(43, 153);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 21);
+            label3.TabIndex = 5;
+            label3.Text = "Пароль";
             // 
             // Логин
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(358, 315);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(textBoxPassword);
+            Controls.Add(textBoxLogin);
             Controls.Add(button1);
             Name = "Логин";
-            Text = "Form1";
+            Text = "Вход";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -86,8 +110,10 @@
         #endregion
 
         private Button button1;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private TextBox textBoxLogin;
+        private TextBox textBoxPassword;
         private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- EBK-EXAM-C#26/Form1.cs
+++ EBK-EXAM-C#26/Form1.cs
@@ -1,15 +1,35 @@
+using EBK_EXAM_C_26.Models;
+
 namespace EBK_EXAM_C_26
 {
-    public partial class Логин : Form
+    public partial class LoginForm : Form
     {
-        public Логин()
+        public static LoginForm Instanse;
+        public static User User { get; set; }
+        public LoginForm()
         {
             InitializeComponent();
+            Instanse = this;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //lol
+            using (KbkExamC26Context db = new KbkExamC26Context())
+            {
+                User user = db.Users.FirstOrDefault(p => p.Login == textBoxLogin.Text && p.Password == textBoxPassword.Text)!;
+
+                if (user != null)
+                {
+                    User = user;
+                    Main mainForm = new Main();
+                    mainForm.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный логин или пароль :(");
+                }
+            }
         }
     }
 }
